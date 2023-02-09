5b5a45ce93f6cbe6122cfcf4e2cf45420c66f1a2
bahargumuser <121288876+bahargumuser@users.noreply.github.com>
2023-02-09 14:42:06 +0300

    :(

--- client/src/App.js
+++ client/src/App.js
@@ -13,13 +13,14 @@ import KaydedilenlerListesi from "./Filmler/KaydedilenlerListesi";
 export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
-  let {id} = useParams();
+  //let {id} = useParams();
 
   useEffect(() => {
     const FilmleriAl = () => {
       axios
         .get("http://localhost:5001/api/filmler") // Burayı Postman'le çalışın
         .then((response) => {
+          setMovieList(response.data);
           // Bu kısmı log statementlarıyla çalışın
           // ve burdan gelen response'u 'movieList' e aktarın
         })
@@ -31,26 +32,27 @@ export default function App() {
   }, []);
 
   const KaydedilenlerListesineEkle = (id) => {
+    console.log("kaydete baktım", id);
+    const newSaved = [...saved, id];
+    setSaved(newSaved);
     // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
   };
 
   return (
     <div>
+      <KaydedilenlerListesi
+        list={
+          [
+            /* Burası esnek */
+          ]
+        }
+      />
       <Switch>
-        <Route path="/FilmListesi">
-          <KaydedilenlerListesi
-            list={
-              [
-                /* Burası esnek */
-              ]
-            }
-          />
-          <div>Bu Div'i kendi Routelarınızla değiştirin</div>
-          <Route
-            path="/filmler/: id">Film ID: {id} </Route>
-      <Route path="
-          />{" "}
-          Anasayfa{" "}
+        <Route>
+          <Film saveCallBack={KaydedilenlerListesineEkle} />
+        </Route>
+        <Route path="/">
+          <FilmListesi movies={movieList} />
         </Route>
       </Switch>
     </div>
